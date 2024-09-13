b05dc81e16b216287413a36ecb90189fb62cf4ab
marioshenrique <juvenciomariohenrique@gmail.com>
2024-09-13 12:09:16 -0300

    dia 4 de estudos

---
+++ ex008/script.js
@@ -0,0 +1,5 @@
+function voltarPagInicial() {
+    window.location.href = "../../../index.html"
+}
+let btn = document.getElementById("btn-voltarpaginainicial")
+btn.addEventListener("click", voltarPagInicial)
